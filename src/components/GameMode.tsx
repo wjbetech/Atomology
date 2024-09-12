ddd4ff56612b15295b41cadc64d5d81e9fae5712
wjbetech <wjbetech@gmail.com>
2024-09-13 02:24:24 +0900

    Fixed some basic styling

--- src/components/GameMode.tsx
+++ src/components/GameMode.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
-import { getRandomElement } from "../hooks/gameLogic.js";
+// import { getRandomElement } from "../hooks/gameLogic.js";
 
-console.log(getRandomElement("2"));
+// console.log(getRandomElement("2"));
 
 type Props = {};
 
@@ -12,13 +12,16 @@ export default function GameMode({}: Props) {
   const handleClick = () => {};
 
   return (
-    <div className="py-14 flex gap-6 justify-center">
-      <button id="multiple" value="multiple" onClick={handleClick} className="btn text-lg">
-        Multiple Choice
-      </button>
-      <button id="open" value="multiple" onClick={handleClick} className="btn text-lg">
-        Open Answer
-      </button>
+    <div className="py-14 flex flex-col gap-4 justify-center">
+      <h1 className="graduate-regular text-5xl">Atomology</h1>
+      <div className="flex justify-center gap-6">
+        <button id="multiple" value="multiple" onClick={handleClick} className="btn text-lg">
+          Multiple Choice
+        </button>
+        <button id="open" value="multiple" onClick={handleClick} className="btn text-lg">
+          Open Answer
+        </button>
+      </div>
     </div>
   );
 }
