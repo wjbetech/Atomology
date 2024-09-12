b4d40be9b8be9191f29c0ec9c78c80568a5cc4ca
wjbetech <wjbetech@gmail.com>
2024-09-13 02:35:28 +0900

    Built a dummy element box with dummy element data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Layout from "./components/Layout";
 import GameMode from "./components/GameMode";
+import Element from "./components/Element";
 
 function App() {
   return (
     <div className="">
       <Layout />
       <GameMode />
+      <Element />
     </div>
   );
 }
--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -7,10 +7,26 @@ interface ElementData {
   symbol: string;
 }
 
+const dummyElement: ElementData = {
+  atomicNumber: "12",
+  name: "Carbon",
+  symbol: "C"
+};
+
 export default function Element({ atomicNumber, name, symbol }: ElementData) {
   const [element, setElement] = useState<ElementData | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<false | null>(null);
 
-  return <div>Element</div>;
+  return (
+    <div className="">
+      {loading && <div>Loading...</div>}
+      {error && <div>Error occurred: {error}</div>}
+      {dummyElement && (
+        <div className="border-2 border-accent h-[200px] place-items-center text-center align-middle justify-center w-[200px] m-auto flex flex-col rounded-sm">
+          <h1 className="text-5xl">{dummyElement.atomicNumber}</h1>
+        </div>
+      )}
+    </div>
+  );
 }
