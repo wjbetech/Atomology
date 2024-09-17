d280af803fdd0249d93fdf50834a39b08cb33e09
wjbetech <wjbetech@gmail.com>
2024-09-17 16:40:44 +0900

    Added dynamic loading state and working on the fetch+processing

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -1,32 +1,47 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { messages } from "../utils/loadingMessages";
 
 interface ElementData {
-  atomicNumber: string | number;
+  atomicNumber: number;
   name: string;
   symbol: string;
 }
 
-const dummyElement: ElementData = {
-  atomicNumber: "12",
-  name: "Carbon",
-  symbol: "C"
+const randomLoadingMessage = () => {
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  return messages[randomIndex];
 };
 
-export default function Element({ atomicNumber, name, symbol }: ElementData) {
-  const [element, setElement] = useState<ElementData | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<false | null>(null);
+export default async function Element({ atomicNumber, name, symbol }: ElementData) {
+  const [elementData, setElementData] = useState({});
+  const [element, setElement] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch("https://kineticzephyr.onrender.com/periodictable")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setElementData(data);
+      });
+  }, []);
+
+  console.log(elementData);
+
+  if (loading) return <p>{randomLoadingMessage()}</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
-    <div className="">
-      {loading && <div>Loading...</div>}
-      {error && <div>Error occurred: {error}</div>}
-      {dummyElement && (
-        <div className="border-4 border-accent h-[200px] place-items-center text-center align-middle justify-center w-[200px] m-auto flex flex-col rounded-md">
-          <h1 className="text-5xl">{dummyElement.atomicNumber}</h1>
-        </div>
-      )}
+    <div className="element">
+      <h2>Element: {element?.name}</h2>
+      <p>Symbol: {element?.symbol}</p>
+      <p>Atomic Number: {element?.atomicNumber}</p>
     </div>
   );
 }
+
+// https://kineticzephyr.onrender.com/periodictable
---
+++ src/utils/loadingMessages.ts
@@ -0,0 +1,16 @@
+export const messages = [
+  "Scouring the universe...",
+  "Shuffling some dirt around...",
+  "Peeking in the lab...",
+  "Smashing up a couple molecules...",
+  "Asking Einstein for his favorite...",
+  "Asking Schrödinger if it exists yet...",
+  "Borrowing from CERN...",
+  "Leafing through a science book...",
+  "Asking my friend Ant-man...",
+  "Ignoring the quantum...",
+  "Balancing some electrons and neurons...",
+  "Picking the right noble one...",
+  "Mining for something...",
+  "Making sure nothing explodes..."
+];
