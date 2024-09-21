13a3e2082f7db4b1af959512785596574fcd87bd
wjbetech <wjbetech@gmail.com>
2024-09-21 15:46:08 +0900

    Fixing more zustand and game-mode

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -8,6 +8,12 @@ import { GET } from "../data/fetch";
 import { useGameStore } from "../store/atomologyStore";
 
 export default function Element() {
+  // get random loading message
+  const randomLoadingMessage = () => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    return messages[randomIndex];
+  };
+
   const {
     element,
     setElement,
@@ -19,21 +25,47 @@ export default function Element() {
     setGameStarted,
   } = useGameStore();
 
-  // get random loading message
-  const randomLoadingMessage = () => {
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    return messages[randomIndex];
-  };
-
   // async GET api call
   useEffect(() => {
     // get random idx for setElementData
     const randomIndex = Math.floor(Math.random() * 119);
+
+    // get the data for one random Element
     const fetchData = async () => {
       try {
         setLoading(true);
         const response = await GET();
-        console.log(response.data.data[randomIndex]);
+        const data = response.data.data[randomIndex];
+
+        // destructure and store data
+        const {
+          atomic_mass: atomicMass,
+          category,
+          density,
+          discovered_by: discoveredBy,
+          melt,
+          name,
+          number,
+          period,
+          phase,
+          symbol,
+        } = data;
+
+        // pass bundled data to zustand
+        setElement({
+          atomicMass,
+          category,
+          density,
+          discoveredBy,
+          melt,
+          name,
+          number,
+          period,
+          phase,
+          symbol,
+        });
+
+        console.log(element);
         setLoading(false);
       } catch (error) {
         setError(error.message);
@@ -49,9 +81,9 @@ export default function Element() {
     if (error) return <p className="mt-24">Error: {error}</p>;
 
     return (
-      <div className="border-4 p-6 border-secondary w-[15%] place-self-center mt-24 rounded-md shadow-md transition-all duration-1000">
-        {/* <span></span> */}
-        <h1 className="font-bold text-3xl">{}</h1>
+      <div className="border-4 p-6 border-secondary w-[66%] place-self-center mt-24 rounded-md shadow-md transition-all duration-1000">
+        <span>{element?.number}</span>
+        <h1 className="font-bold text-3xl">{element?.symbol}</h1>
       </div>
     );
   }
