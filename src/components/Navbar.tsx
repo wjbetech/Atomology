bce7134ca8742d222fcf7c0e149ae44dfe281ba2
wjbetech <wjbetech@gmail.com>
2024-09-23 11:24:03 +0900

    fixed links in wrong pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
+// react-router for useLocation
+import { Link, useLocation } from "react-router-dom";
+
 type Props = {};
 
 export default function Navbar({}: Props) {
+  // get current page
+  const location = useLocation();
+
   return (
-    <nav className="p-6 flex fixed w-screen justify-center">
-      <div className="flex gap-4">
-        <a href="/">Main</a>
-        <a href="/about">About</a>
-        <a href="/faq">FAQ</a>
-      </div>
+    <nav className="p-6 flex fixed w-screen justify-center gap-4">
+      {/* make sure pages don't load links to themselves */}
+      {location.pathname !== "/" && <Link to="/">Main</Link>}
+      {location.pathname !== "/about" && <Link to="/about">About</Link>}
+      {location.pathname !== "/faq" && <Link to="/faq">FAQ</Link>}
     </nav>
   );
 }
