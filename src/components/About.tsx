060af47601bac22ab06deb66c919bf59601b00c2
wjbetech <wjbetech@gmail.com>
2024-09-25 20:37:25 +0900

    Added a brief about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,8 +4,25 @@ type Props = {};
 
 export default function About({}: Props) {
   return (
-    <div>
-      <h1>Welcome to the About page</h1>
+    <div className="flex flex-col gap-y-10 w-[500px] text-center text-lg place-self-center">
+      <p>
+        The idea for Atomology formed from a penchant for the game Wordle, and
+        my participation in the Seoul Tech Impact Hackathon hosted in September
+        of 2024.
+      </p>
+      <p>
+        I wanted to create a game that would gently tease the players knowledge,
+        without the burden of complexity or unnecessary competitiveness -
+        something I think that Wordle also does brilliantly.
+      </p>
+      <p>
+        I hope that players feel they can "drop in" to playing Atomology, give
+        it a few rounds, get the dopamine hit they were craving, and then put it
+        down or close it away without a sense of missing out, compulsion to
+        continue, or over-disappointment if they happened to guess the element
+        incorrectly.
+      </p>
+      <p>Have fun everyone!</p>
     </div>
   );
 }
