dd43beef5fb3a5b66355a9ad1dc436f5fe9e6757
wjbetech <wjbetech@gmail.com>
2024-10-22 21:02:28 +0900

    fixed link to github

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ type Props = {};
 export default function Footer({}: Props) {
   return (
     <div className="p-4 flex flex-row gap-10 justify-center">
-      <Link to="www.github.com/wjbetech">@wjbetech</Link>
+      <Link to="https://github.com/wjbetech">@wjbetech</Link>
     </div>
   );
 }
